0d14ed2a3fb3854c3efdb88eee1dd3270c0f04f9
Jose Guzman <jg175415@gmail.com>
2022-02-27 14:40:32 -0500

    feat: screen design finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 'strict mode';
 
-console.log("Probando");
+// function buttonShadow(button) {
+//     console.log("Pulsado")
+//     console.log(this);
+//     button.style.boxShadow = "inset 0px 0px 2px 2px black";
+// }
